831bb10ee00879a936a0b3c7f4231ae540e9a867
TakumuKobayashi <utm2577206@stu.o-hara.ac.jp>
2026-08-03 01:31:17 +0900

    定時のアニメーション制作
    アニメーション制作していたらこんな時間。やり始めるの遅すぎ！

---
+++ Kobayashi/Assets/script/TeijiMove.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class TeijiMove : MonoBehaviour
+{
+    public GameManager GM;
+    public ScoreManager SM;
+
+    public TextMeshProUGUI ScoreUI;
+    public TextMeshProUGUI BonusUI;
+    public TextMeshProUGUI BairituUI;
+
+    public TextMeshProUGUI ScoreDummyUI;
+    public TextMeshProUGUI BonusDummyUI;
+    public TextMeshProUGUI BairituDummyUI;
+
+    
+}
